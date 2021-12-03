ca3bb4c12f52416b7ecbbd8fa402fb503661657e
Bruno <brunobilheri@hotmail.com>
2021-12-02 23:52:39 -0300

    Passagem valor referencia

--- src/passagemReferencia/TesteCarro.java
+++ src/passagemReferencia/TesteCarro.java
@@ -5,11 +5,13 @@ public class TesteCarro {
         Carro carro1 = new Carro("vermelho", "fiat toro", 2015);
         Carro carro2 = new Carro("branco", "gol", 1995);
 
+        System.out.println(carro1.hashCode());
         carro1.exibeDados();
         System.out.println("-----------------------");
-
+        System.out.println(carro1.hashCode());
         carro1 = carro2;
 
         carro1.exibeDados();
+        System.out.println(carro1.hashCode());
     }
 }
