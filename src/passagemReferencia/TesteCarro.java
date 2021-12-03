f07c75800596cf4623db3fdf606ad958ccf46aad
Bruno <brunobilheri@hotmail.com>
2021-12-03 00:01:01 -0300

    apontando para referencia do objeto 2

--- src/passagemReferencia/TesteCarro.java
+++ src/passagemReferencia/TesteCarro.java
@@ -4,11 +4,14 @@ public class TesteCarro {
     public static void main(String[] args) {
         Carro carro1 = new Carro("vermelho", "fiat toro", 2015);
         Carro carro2 = new Carro("branco", "gol", 1995);
-
+        // Referencia memória objeto1
         System.out.println(carro1.hashCode());
+
+        // Referencia memória objeto2
+        System.out.println(carro2.hashCode());
+
         carro1.exibeDados();
         System.out.println("-----------------------");
-        System.out.println(carro1.hashCode());
         carro1 = carro2;
 
         carro1.exibeDados();
